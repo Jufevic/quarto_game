d69704608c83b80335323a97aea2f295c0aba7a9
Jufevic <60904290+Jufevic@users.noreply.github.com>
2025-03-28 00:53:45 +0100

    Solved various bugs, added self

--- game.py
+++ game.py
@@ -1,11 +1,8 @@
 """Main game logic."""
 
-from itertools import product
-from random import choice, shuffle
-from string import ascii_uppercase as alphabet
+from random import shuffle
 
 from board import Board
-from piece import Piece
 from player import HumanPlayer, Player, RandomRobotPlayer
 
 
@@ -18,7 +15,9 @@ def play_game(player1: Player, player2: Player):
     board = Board()
     winner = None
     # Randomly select the first player
-    piece_chooser, position_chooser = shuffle([player1, player2])
+    players = [player1, player2]
+    shuffle(players)
+    piece_chooser, position_chooser = players
 
     # Every turn is divided in two steps: a player chooses a piece, and its
     # opponent chooses wher to put it
@@ -49,6 +48,8 @@ def play_game(player1: Player, player2: Player):
         piece_chooser, position_chooser = position_chooser, piece_chooser
 
     # Game is finished, declare who won
+    board.display()
+    print("Game finished")
     if winner is not None:
         print(f"The winner is {winner.name}!")
     else:
@@ -63,83 +64,84 @@ def play_against_machine():
 
 
 if __name__ == "__main__":
-    board = Board()
-    finished = False
-    winner = None
-    your_turn = True
-    while not finished:
-        board.display()
-        if your_turn:
-            if not board.available_pieces:
-                finished = True
-                break
-            print("\nYour turn")
-            # Choose a piece to give to the opponent
-            chosen = None
-            while chosen is None:
-                pieces = ", ".join(f"{piece:04b}" for piece in board.available_pieces)
-                print(f"Available pieces: {pieces}")
-                user_input = input("Choose a number from the available numbers: ")
-                try:
-                    chosen = Piece(int(user_input, 2))
-                except ValueError:
-                    chosen = None
-                    print("Invalid choice.")
-                if isinstance(chosen, Piece) and chosen not in board.available_pieces:
-                    print("Piece is already on the board.")
-                    chosen = None
-            print(f"Chosen piece: {chosen:04b}")
+    # board = Board()
+    # finished = False
+    # winner = None
+    # your_turn = True
+    # while not finished:
+    #     board.display()
+    #     if your_turn:
+    #         if not board.available_pieces:
+    #             finished = True
+    #             break
+    #         print("\nYour turn")
+    #         # Choose a piece to give to the opponent
+    #         chosen = None
+    #         while chosen is None:
+    #             pieces = ", ".join(f"{piece:04b}" for piece in board.available_pieces)
+    #             print(f"Available pieces: {pieces}")
+    #             user_input = input("Choose a number from the available numbers: ")
+    #             try:
+    #                 chosen = Piece(int(user_input, 2))
+    #             except ValueError:
+    #                 chosen = None
+    #                 print("Invalid choice.")
+    #             if isinstance(chosen, Piece) and chosen not in board.available_pieces:
+    #                 print("Piece is already on the board.")
+    #                 chosen = None
+    #         print(f"Chosen piece: {chosen:04b}")
 
-            # Put the piece to a random valid location on the grid
-            empties = {
-                (row, col)
-                for row, col in product(range(4), repeat=2)
-                if board.grid[row][col] < 0
-            }
-            row, col = choice(tuple(empties))
-            print(f"Chosen position: {alphabet[row]}{col}")
-            board.put_piece(chosen, (row, col))
-            if board.is_game_finished():
-                finished = True
-                winner = "your opponent"
-                break
+    #         # Put the piece to a random valid location on the grid
+    #         empties = {
+    #             (row, col)
+    #             for row, col in product(range(4), repeat=2)
+    #             if board.grid[row][col] < 0
+    #         }
+    #         row, col = choice(tuple(empties))
+    #         print(f"Chosen position: {alphabet[row]}{col}")
+    #         board.put_piece(chosen, (row, col))
+    #         if board.is_game_finished():
+    #             finished = True
+    #             winner = "your opponent"
+    #             break
 
-        # Opponent's turn
-        else:
-            if not board.available_pieces:
-                finished = True
-                break
-            print("\nOpponent's turn")
-            chosen = choice(tuple(board.available_pieces))
-            print(f"Chosen piece: {chosen:04b}")
+    #     # Opponent's turn
+    #     else:
+    #         if not board.available_pieces:
+    #             finished = True
+    #             break
+    #         print("\nOpponent's turn")
+    #         chosen = choice(tuple(board.available_pieces))
+    #         print(f"Chosen piece: {chosen:04b}")
 
-            # Put the piece to a valid location on the grid
-            row, col = None, None
-            while row is None or col is None:
-                empties = [f"{alphabet[r]}{c}" for r, c in board.empty_positions]
-                print(f"Available empty positions: {', '.join(empties)}")
-                user_input = input("Choose a position from the available positions: ")
-                try:
-                    row, col = alphabet.index(user_input[0]), int(user_input[1])
-                except (ValueError, KeyError):
-                    row, col = None, None
-                    print("Invalid choice.")
-                if user_input not in empties:
-                    row, col = None, None
-            print(f"Chosen position: {alphabet[row]}{col}")
-            board.put_piece(chosen, (row, col))
-            if board.is_game_finished():
-                finished = True
-                winner = "you"
-                break
-        your_turn = not your_turn
+    #         # Put the piece to a valid location on the grid
+    #         row, col = None, None
+    #         while row is None or col is None:
+    #             empties = [f"{alphabet[r]}{c}" for r, c in board.empty_positions]
+    #             print(f"Available empty positions: {', '.join(empties)}")
+    #             user_input = input("Choose a position from the available positions: ")
+    #             try:
+    #                 row, col = alphabet.index(user_input[0]), int(user_input[1])
+    #             except (ValueError, KeyError):
+    #                 row, col = None, None
+    #                 print("Invalid choice.")
+    #             if user_input not in empties:
+    #                 row, col = None, None
+    #         print(f"Chosen position: {alphabet[row]}{col}")
+    #         board.put_piece(chosen, (row, col))
+    #         if board.is_game_finished():
+    #             finished = True
+    #             winner = "you"
+    #             break
+    #     your_turn = not your_turn
 
-    # Game is finished, declare who is the winner and why
-    board.display()
-    print("Game finished!")
-    if winner == "you":
-        print("Congratulations! You won! 😁")
-    elif winner == "your opponent":
-        print("You loose! Booh ☹️")
-    elif winner is None:
-        print("Tie!")
+    # # Game is finished, declare who is the winner and why
+    # board.display()
+    # print("Game finished!")
+    # if winner == "you":
+    #     print("Congratulations! You won! 😁")
+    # elif winner == "your opponent":
+    #     print("You loose! Booh ☹️")
+    # elif winner is None:
+    #     print("Tie!")
+    play_against_machine()
--- player.py
+++ player.py
@@ -2,6 +2,7 @@
 
 from abc import ABC, abstractmethod
 from random import choice
+from string import ascii_uppercase as alphabet
 
 from board import Board
 from piece import Piece
@@ -9,25 +10,57 @@ from piece import Piece
 
 class Player(ABC):
     def __init__(self, name="default name"):
-        super().__init__(name=name)
+        self.name = name
 
     @abstractmethod
-    def choose_piece(board: Board) -> Piece:
+    def choose_piece(self, board: Board) -> Piece:
         """Choose a piece from the set of available pieces to give to the opponent."""
         pass
 
     @abstractmethod
-    def choose_position(piece: Piece, board: Board) -> tuple[int, int]:
+    def choose_position(self, piece: Piece, board: Board) -> tuple[int, int]:
         """Choose where to put a given piece on the board."""
         pass
 
 
 class HumanPlayer(Player):
-    def choose_piece(board):
+    def choose_piece(self, board):
         board.display()
+        print(f"\n{self.name}'s turn")
+        # Choose a piece to give to the opponent
+        chosen = None
+        while chosen is None:
+            pieces = ", ".join(f"{piece:04b}" for piece in board.available_pieces)
+            print(f"Available pieces: {pieces}")
+            user_input = input("Choose a number from the available numbers: ")
+            try:
+                chosen = Piece(int(user_input, 2))
+            except ValueError:
+                chosen = None
+                print("Invalid choice.")
+            if isinstance(chosen, Piece) and chosen not in board.available_pieces:
+                print(f"Piece {chosen:04b} is already on the board.")
+                chosen = None
+        print(f"Chosen piece: {chosen:04b}")
+        return chosen
 
-    def choose_position(board):
+    def choose_position(self, piece, board):
         board.display()
+        print(f"Piece chosen by your opponent: {piece:04b}")
+        row, col = None, None
+        while row is None or col is None:
+            empties = [f"{alphabet[r]}{c}" for r, c in board.empty_positions]
+            print(f"Available empty positions: {', '.join(empties)}")
+            user_input = input("Choose a position from the available positions: ")
+            try:
+                row, col = alphabet.index(user_input[0]), int(user_input[1])
+            except (ValueError, KeyError):
+                row, col = None, None
+                print("Invalid choice.")
+            if user_input not in empties:
+                row, col = None, None
+        print(f"Chosen position: {alphabet[row]}{col}")
+        return row, col
 
 
 class RobotPlayer(Player):
@@ -35,8 +68,8 @@ class RobotPlayer(Player):
 
 
 class RandomRobotPlayer(RobotPlayer):
-    def choose_piece(board):
+    def choose_piece(self, board):
         return choice(tuple(board.available_pieces))
 
-    def choose_position(board):
+    def choose_position(self, piece, board):
         return choice(tuple(board.empty_positions))
